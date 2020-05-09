d75cbde53f1fa795b8bb6956d10a4781a7ccf781
sarthakarora1208 <sarthakarora1208@gmail.com>
2020-05-09 21:36:59 +0530

    fixed routes

--- frontend/server.js
+++ frontend/server.js
@@ -31,11 +31,11 @@ app.use(cookieParser());
 
 // Express session
 app.use(
-	session({
-		secret: 'secret',
-		resave: true,
-		saveUninitialized: true
-	})
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+  })
 );
 // Connect flash
 app.use(flash());
@@ -53,27 +53,32 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Global variables
-app.use(function(req, res, next) {
-	res.locals.success_msg = req.flash('success_msg');
-	res.locals.error_msg = req.flash('error_msg');
-	res.locals.error = req.flash('error');
-	next();
+app.use(function (req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
 });
 
 // controllers
 app.get('/', (req, res) => res.render('index'));
 app.get('/verify', (req, res) => {
-	const queryObject = url.parse(req.url, true).query;
-	//console.log(queryObject.address)
-	res.render('verify-address', { address: queryObject.address });
+  const queryObject = url.parse(req.url, true).query;
+  //console.log(queryObject.address)
+  res.render('verify-address', { address: queryObject.address });
 });
 app.get('/qrscanner', (req, res) => res.render('qrscanner'));
 app.get('/contactUs', (req, res) => res.render('contactUs'));
-app.get('/patient-data-success', (req, res) => res.render('patient-data-success'));
-app.get('/patient-data-failure', (req, res) => res.render('patient-data-failure'));
-app.get('/auth/user:register', (req, res) => res.render('user:register'));
-app.get('/auth/hospital:register', (req, res) => res.render('hospital:register'));
-app.get('/display-keys', (req, res) => res.render('display-keys'));
+app.get('/patient-data-success', (req, res) =>
+  res.render('patient-data-success')
+);
+app.get('/patient-data-failure', (req, res) =>
+  res.render('patient-data-failure')
+);
+app.get('/auth/user-register', (req, res) => res.render('user-register'));
+app.get('/auth/hospital-register', (req, res) =>
+  res.render('hospital-register')
+);
 
 // Mount routers
 app.use('/auth', authFrontend);
@@ -83,11 +88,14 @@ app.use('/admin', adminFrontend);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-	console.log(`Error: ${err.message}`);
-	// Close server & exit process
-	// server.close(() => process.exit(1));
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  // server.close(() => process.exit(1));
 });
