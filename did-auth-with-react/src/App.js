e75983cbc1ff76047a3661f5eb40bccdc3ba725a
sarthakarora1208 <sarthakarora1208@gmail.com>
2020-05-12 16:55:09 +0530

    config.env

--- did-auth-with-react/src/App.js
+++ did-auth-with-react/src/App.js
@@ -39,36 +39,6 @@ function App() {
   return (
     <div className="App">
       <header id="showcase">
-        <div class="navbar-fixed">
-            <nav class="none">
-                <div class="container">
-
-                    <div class="nav-wrapper">
-
-                        <a href="#" class="brand-logo">
-                            Well<span style="color: #8EE4AF">Pass</span></a>
-                        <a href="#" class="sidenav-trigger" data-target="mobile-nav">
-                            <i class="material-icons">menu</i>
-                        </a>
-
-                        <ul class="right hide-on-med-and-down ">
-                            <li><a href="#">Logged in as {session.user.name}
-                                </a></li>
-                            <li><a href="/auth/logout">Logout</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
-
-        <ul class="sidenav" id="mobile-nav">
-            <li>
-                <a style="color:white;" href="#">
-                    Logged in as {session.user.name}
-                </a>
-            </li>
-            <li><a style="color:white;" href="/auth/logout">Logout</a></li>
-        </ul>
         {sessionMarkup}
       </header>
     </div>
